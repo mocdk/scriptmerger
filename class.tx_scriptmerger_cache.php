c96fcccb5577088140a09c3e0ecfce5f27067a8b
www-data <www-data@mocsystems.com>
2011-06-14 13:49:56 +0200

    Initial import of modifled scriptmerger

---
+++ class.tx_scriptmerger_cache.php
@@ -0,0 +1,110 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2010 Stefan Galinski (stefan.galinski@gmail.com)
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*  A copy is found in the textfile GPL.txt and important notices to the license
+*  from the author is found in LICENSE.txt distributed with these scripts.
+*
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * This class contains a class which contains a hook method
+ * for the "clear all cache" action in the TYPO3 backend.
+ *
+ * @author Stefan Galinski <stefan.galinski@gmail.com>
+ */
+
+/**
+ * This class contains a hook method for the "clear all cache" action in the TYPO3 backend.
+ *
+ * @author Stefan Galinski <stefan.galinski@gmail.com>
+ */
+class tx_scriptmerger_cache {
+	/**
+	 * Contains the temporary directories of this extension.
+	 *
+	 *  @var array
+	 */
+	protected $tempDirectories = array();
+
+	/**
+	 * Initializes some class variables...
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		// define temporary directories
+		$this->tempDirectories = array (
+			PATH_site . 'typo3temp/scriptmerger/' => 0,
+			PATH_site . 'typo3temp/scriptmerger/temp/' => 0,
+			PATH_site . 'typo3temp/scriptmerger/uncompressed/' => 1209600,
+			PATH_site . 'typo3temp/scriptmerger/compressed/' => 1209600
+		);
+	}
+
+	/**
+	 * Clear cache post processor
+	 *
+	 * This method deletes all temporary files.
+	 *
+	 * @param object $params parameter array
+	 * @param object $pObj parent object
+	 * @return void
+	 */
+	public function clearCachePostProc(&$params, &$pObj) {
+		// only if the cache command is available
+		if ($params['cacheCmd'] !== 'all') {
+			return;
+		}
+
+		// remove any files in the directories array that are older than one month
+		$now = time();
+		foreach ($this->tempDirectories as $tempDirectory => $maxAge) {
+			if (!is_dir($tempDirectory)) {
+				continue;
+			}
+
+			$handle = opendir($tempDirectory);
+			while (false !== ($file = readdir($handle))) {
+
+				if ($file == '.' || $file == '..') {
+					continue;
+				}
+
+				if (is_file($tempDirectory . $file)) {
+					// get last modification time
+					$lastAccess = fileatime($tempDirectory . $file);
+					$age = $now - $lastAccess;
+
+					if ($age >= $maxAge) {
+						unlink($tempDirectory . $file);
+					}
+				}
+			}
+		}
+	}
+}
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/scriptmerger/class.tx_scriptmerger_cache.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/scriptmerger/class.tx_scriptmerger_cache.php']);
+}
+
+?>
---
+++ ext_emconf.php
@@ -0,0 +1,52 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "scriptmerger".
+#
+# Auto generated 08-04-2011 16:03
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'CSS/Javascript Minificator, Compressor And Merger',
+	'description' => 'This extension minimizes the http requests by a simple merging of your css and javascript. The result files will be minified and compressed. Anything is highly configurable. The extension make usage of the "minify" and "jsminplus" projects.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '3.1.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => 'bottom',
+	'loadOrder' => 'tstidy',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => 'typo3temp/scriptmerger/',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Stefan Galinski',
+	'author_email' => 'stefan.galinski@gmail.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.1-5.3.99',
+			'typo3' => '4.2.0-4.4.99',
+		),
+		'conflicts' => array(
+			'speedy' => '',
+			'queo_speedup' => '',
+			'js_css_optimizer' => '',
+			'minify' => '',
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:25:"class.tx_scriptmerger.php";s:4:"2c02";s:31:"class.tx_scriptmerger_cache.php";s:4:"717d";s:16:"example.htaccess";s:4:"c522";s:12:"ext_icon.gif";s:4:"2cfb";s:17:"ext_localconf.php";s:4:"1455";s:14:"ext_tables.php";s:4:"fa00";s:27:"configuration/constants.txt";s:4:"371a";s:23:"configuration/setup.txt";s:4:"23c8";s:14:"doc/manual.sxw";s:4:"b855";s:23:"resources/jsminplus.php";s:4:"48ed";s:35:"resources/minify/lib/Minify/CSS.php";s:4:"dc9e";s:48:"resources/minify/lib/Minify/CommentPreserver.php";s:4:"86ba";s:47:"resources/minify/lib/Minify/ImportProcessor.php";s:4:"5ce5";s:46:"resources/minify/lib/Minify/CSS/Compressor.php";s:4:"b514";s:47:"resources/minify/lib/Minify/CSS/UriRewriter.php";s:4:"5ab5";}',
+);
+
+?>
